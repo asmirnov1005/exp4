e78a383bc795d98c8925207ee9a0238ac1930010
Artyom Smirnov <asmirnov1005@gmail.com>
2021-02-02 23:01:06 +0300

    Refactor Peephole class.

--- js/peephole.js
+++ js/peephole.js
@@ -1,138 +1,156 @@
 class Peephole {
 
   constructor(imgElem, displayElem, options) {
-    this.touchClientPosition = null;
-    this.touchNaturalPosition = null;
-    this.naturalPosition = null;
-
     this.speed = options.speed || 1;
-    this.showGlass = options.showGlass === false ? false : true;
 
-    this.imgElem = imgElem;
-    this.nW = this.imgElem.naturalWidth;
-    this.nH = this.imgElem.naturalHeight;
-    this.cW = this.imgElem.clientWidth;
-    this.cH = this.imgElem.clientHeight;
-    if (!this.cW || !this.cH)
-      throw new Error("Image element size (width and height) should be specified!");
-
-    this.displayElem = displayElem;
-    this.dcW = this.displayElem.clientWidth;
-    this.dcH = this.displayElem.clientHeight;
-    if (!this.dcW || !this.dcH)
-      throw new Error("Display element size (width and height) should be specified!");
-    if (!options.displaySize)
-      throw new Error("`displaySize` is required option!");
-    this.dnW = options.displaySize.width;
-    this.dnH = options.displaySize.height;
-    // TODO: displaySize.w, displaySize.h should be === 1 mod 2
-    // TODO: this.dcW / this.dnW should be integer
-    if (!this.dnW || !this.dnH)
-      throw new Error("Display size (dW and dH parameters) should be nonzero integers!");
-    this.displayElem.style.backgroundRepeat = "no-repeat";
-    this.displayElem.style.backgroundSize = this.nW * Math.round(this.dcW / this.dnW) + "px " + this.nH * Math.round(this.dcH / this.dnH) + "px";
-  	this.displayElem.style.imageRendering = "-moz-crisp-edges";
-    this.displayElem.style.imageRendering = "-webkit-crisp-edges";
-    this.displayElem.style.imageRendering = "pixelated";
-    this.displayElem.style.imageRendering = "crisp-edges";
-
-    if (this.showGlass) {
-      this.mnW = this.dnW;
-      this.mnH = this.dnH;
-      this.mcW = Math.round(this.mnW * this.cW / this.nW);
-      this.mcH = Math.round(this.mnH * this.cH / this.nH);
-      this.glassElem = document.createElement("div");
-      this.glassElem.classList.add("peephole-glass");
-      this.glassElem.style.display = "none";
-      this.glassElem.style.position = "absolute";
-      this.glassElem.style.width = this.mcW + "px";
-      this.glassElem.style.height = this.mcH + "px";
-      this.glassElem.style.border = "solid black 1px";
-      this.imgElem.parentElement.insertBefore(this.glassElem, this.imgElem);
-    }
-
+    this._initImage(imgElem);
+    this._initDisplay(displayElem, options.displaySize);
+    this._initGlass(options.showGlass);
     this._initEvents(options.events || {});
+    this._setupPositions();
   }
 
   log() {
-    console.log('Natural Size', this.nW, this.nH);
-    console.log('Image Size', this.cW, this.cH);
-    console.log('Display Natural Size', this.dnW, this.dnH);
-    console.log('Display Element Size', this.dcW, this.dcH);
-    if (this.showGlass) {
-      console.log('Glass Natural Size', this.mnW, this.mnH);
-      console.log('Glass Element Size', this.mcW, this.mcH);
+    console.log('Natural Size', this._nW, this._nH);
+    console.log('Image Size', this._cW, this._cH);
+    console.log('Display Natural Size', this._dnW, this._dnH);
+    console.log('Display Element Size', this._dcW, this._dcH);
+    if (this._showGlass) {
+      console.log('Glass Natural Size', this._gnW, this._gnH);
+      console.log('Glass Element Size', this._gcW, this._gcH);
     }
   }
 
+  _setupPositions() {
+    this._touchClientPosition = null;
+    this._touchNaturalPosition = null;
+    this._naturalPosition = null;
+  }
+
+  _initImage(imgElem) {
+    this._imgElem = imgElem;
+
+    this._nW = this._imgElem.naturalWidth;
+    this._nH = this._imgElem.naturalHeight;
+    this._cW = this._imgElem.clientWidth;
+    this._cH = this._imgElem.clientHeight;
+    if (!this._cW || !this._cH)
+      throw new Error("Image element size (width and height) should be specified!");
+  }
+
+  _initDisplay(displayElem, displaySize) {
+    this._displayElem = displayElem;
+
+    this._dcW = this._displayElem.clientWidth;
+    this._dcH = this._displayElem.clientHeight;
+    if (!this._dcW || !this._dcH)
+      throw new Error("Display element size (width and height) should be specified!");
+    if (!displaySize)
+      throw new Error("`displaySize` is required option!");
+    this._dnW = displaySize.width;
+    this._dnH = displaySize.height;
+    if (this._dnW % 2 !== 1 || this._dnH % 2 !== 1)
+      throw new Error("Width and height of the display should be odd integers!");
+    if (this._dcW % this._dnW !== 0 || this._dcH % this._dnH !== 0)
+      throw new Error("Width and height of the display element should be divisible by its natural width and height.");
+    if (!this._dnW || !this._dnH)
+      throw new Error("Display size (dW and dH parameters) should be nonzero integers!");
+
+    this._displayElem.style.backgroundRepeat = "no-repeat";
+    this._displayElem.style.backgroundSize = this._nW * Math.round(this._dcW / this._dnW) + "px " + this._nH * Math.round(this._dcH / this._dnH) + "px";
+  	this._displayElem.style.imageRendering = "-moz-crisp-edges";
+    this._displayElem.style.imageRendering = "-webkit-crisp-edges";
+    this._displayElem.style.imageRendering = "pixelated";
+    this._displayElem.style.imageRendering = "crisp-edges";
+  }
+
+  _initGlass(showGlass) {
+    this._showGlass = showGlass === false ? false : true;
+
+    if (!this._showGlass) return;
+
+    this._gnW = this._dnW;
+    this._gnH = this._dnH;
+    this._gcW = Math.round(this._gnW * this._cW / this._nW);
+    this._gcH = Math.round(this._gnH * this._cH / this._nH);
+
+    this._glassElem = document.createElement("div");
+    this._glassElem.classList.add("peephole-glass");
+    this._glassElem.style.display = "none";
+    this._glassElem.style.position = "absolute";
+    this._glassElem.style.width = this._gcW + "px";
+    this._glassElem.style.height = this._gcH + "px";
+    this._glassElem.style.border = "solid black 1px";
+
+    this._imgElem.parentElement.insertBefore(this._glassElem, this._imgElem);
+  }
+
   _initEvents(events) {
-    this.onMousedown = events.onMousedown;
-    this.onMouseup = events.onMouseup;
-    this.imgElem.addEventListener("mousedown", this._imgElemOnMousedownEvent.bind(this));
-    this.imgElem.addEventListener("touchstart", this._imgElemOnMousedownEvent.bind(this));
-    if (this.showGlass) {
-      this.glassElem.addEventListener("mousedown", this._imgElemOnMousedownEvent.bind(this));
-      this.glassElem.addEventListener("touchstart", this._imgElemOnMousedownEvent.bind(this));
+    this._onMousedown = events.onMousedown;
+    this._onMouseup = events.onMouseup;
+
+    this._imgElem.addEventListener('load', this._imgElemOnLoad.bind(this));
+    this._imgElem.addEventListener("mousedown", this._imgElemOnMousedownEvent.bind(this));
+    this._imgElem.addEventListener("touchstart", this._imgElemOnMousedownEvent.bind(this));
+    if (this._showGlass) {
+      this._glassElem.addEventListener("mousedown", this._imgElemOnMousedownEvent.bind(this));
+      this._glassElem.addEventListener("touchstart", this._imgElemOnMousedownEvent.bind(this));
     }
     document.addEventListener("mousemove", this._onPeepholeMoveEvent.bind(this));
     document.addEventListener("touchmove", this._onPeepholeMoveEvent.bind(this));
     document.addEventListener("mouseup", this._documentOnMouseupEvent.bind(this));
     document.addEventListener("touchend", this._documentOnMouseupEvent.bind(this));
-    this.imgElem.addEventListener('load', this._imgElemOnLoad.bind(this));
   }
 
   _imgElemOnMousedownEvent(event) {
     event.preventDefault();
 
-    this.touchClientPosition = this._cursorPosition(event);
-    this.touchNaturalPosition = {
-      x : Math.round(this.touchClientPosition.x * this.nW / this.cW),
-      y : Math.round(this.touchClientPosition.y * this.nH / this.cH),
+    this._touchClientPosition = this._cursorPosition(event);
+    this._touchNaturalPosition = {
+      x : Math.round(this._touchClientPosition.x * this._nW / this._cW),
+      y : Math.round(this._touchClientPosition.y * this._nH / this._cH),
     }
-    if (this.onMousedown)
+    if (this._onMousedown)
       setTimeout(function (x, y) {
-        this.onMousedown(event, { x : x, y : y });
-      }.bind(this, this.touchNaturalPosition.x, this.touchNaturalPosition.y));
-    if (this.showGlass)
-      this.glassElem.style.display = "block";
-    this.displayElem.style.backgroundImage = "url('" + this.imgElem.src + "')";
+        this._onMousedown(event, { x : x, y : y });
+      }.bind(this, this._touchNaturalPosition.x, this._touchNaturalPosition.y));
+    if (this._showGlass)
+      this._glassElem.style.display = "block";
+    this._displayElem.style.backgroundImage = "url('" + this._imgElem.src + "')";
     this._onPeepholeMoveEvent(event);
   }
 
   _onPeepholeMoveEvent(event) {
     event.preventDefault();
 
-    if (!this.touchClientPosition) return;
-    const clientOrigin = this.touchClientPosition;
+    if (!this._touchClientPosition) return;
+    const clientOrigin = this._touchClientPosition;
     const clientCurrentTouch = this._cursorPosition(event);
     const clientDX = clientCurrentTouch.x - clientOrigin.x;
     const clientDY = clientCurrentTouch.y - clientOrigin.y;
     const naturalDX = Math.round(clientDX * this.speed);
     const naturalDY = Math.round(clientDY * this.speed);
-    const naturalX = Math.max(0, Math.min(this.nW - 1, this.touchNaturalPosition.x + naturalDX));
-    const naturalY = Math.max(0, Math.min(this.nH - 1, this.touchNaturalPosition.y + naturalDY));
-    this.naturalPosition = { x : naturalX, y : naturalY };
+    const naturalX = Math.max(0, Math.min(this._nW - 1, this._touchNaturalPosition.x + naturalDX));
+    const naturalY = Math.max(0, Math.min(this._nH - 1, this._touchNaturalPosition.y + naturalDY));
+    this._naturalPosition = { x : naturalX, y : naturalY };
     const clientPosition = {
-      x : Math.round(this.naturalPosition.x * this.cW / this.nW) - 1,
-      y : Math.round(this.naturalPosition.y * this.cH / this.nH) - 1,
+      x : Math.round(this._naturalPosition.x * this._cW / this._nW) - 1,
+      y : Math.round(this._naturalPosition.y * this._cH / this._nH) - 1,
     }
-    if (this.showGlass)
+    if (this._showGlass)
       this._updateglassElemPosition(clientPosition);
-
-    this._updateDisplay(this.naturalPosition);
+    this._updateDisplay(this._naturalPosition);
   }
 
   _documentOnMouseupEvent(event) {
     event.preventDefault();
 
-    if (!this.touchClientPosition) return;
-    if (this.onMouseup)
+    if (!this._touchClientPosition) return;
+    if (this._onMouseup)
       setTimeout(function (x, y) {
-        this.onMouseup(event, { x : x, y : y });
-      }.bind(this, this.naturalPosition.x, this.naturalPosition.y));
-    this.touchClientPosition = null;
-    this.touchNaturalPosition = null;
-    this.naturalPosition = null;
+        this._onMouseup(event, { x : x, y : y });
+      }.bind(this, this._naturalPosition.x, this._naturalPosition.y));
+    this._setupPositions();
   }
 
   _cursorPosition(event) {
@@ -141,30 +159,28 @@ class Peephole {
       const touches = event.touches || event.changedTouches;
       event = touches[0];
     }
-    const rect = this.imgElem.getBoundingClientRect();
-    let x = event.pageX - rect.left;
-    let y = event.pageY - rect.top;
-    x = x - window.pageXOffset;
-    y = y - window.pageYOffset;
+    const rect = this._imgElem.getBoundingClientRect();
+    const x = event.pageX - rect.left - window.pageXOffset;
+    const y = event.pageY - rect.top - window.pageYOffset;
     return { x : x, y : y };
   }
 
   _imgElemOnLoad(event) {
-    if (this.displayElem.style.backgroundImage != "")
-      this.displayElem.style.backgroundImage = "url('" + this.imgElem.src + "')";
+    if (this._displayElem.style.backgroundImage != "")
+      this._displayElem.style.backgroundImage = "url('" + this._imgElem.src + "')";
   }
 
   _updateglassElemPosition(position) {
-    this.glassElem.style.left = (position.x + this.mcW / 2) + "px";
-    this.glassElem.style.top = (position.y + this.mcH / 2) + "px";
+    this._glassElem.style.left = (position.x + this._gcW / 2) + "px";
+    this._glassElem.style.top = (position.y + this._gcH / 2) + "px";
   }
 
   _updateDisplay(position) {
-    const dx = (position.x - Math.floor(this.dnW / 2)) * Math.round(this.dcW / this.dnW);
-    const dy = (position.y - Math.floor(this.dnH / 2)) * Math.round(this.dcH / this.dnH);
+    const dx = (position.x - Math.floor(this._dnW / 2)) * Math.round(this._dcW / this._dnW);
+    const dy = (position.y - Math.floor(this._dnH / 2)) * Math.round(this._dcH / this._dnH);
     const sDX = dx > 0 ? "-" + dx + "px" : -dx + "px";
     const sDY = dy > 0 ? "-" + dy + "px" : -dy + "px";
-    this.displayElem.style.backgroundPosition = sDX + " " + sDY;
+    this._displayElem.style.backgroundPosition = sDX + " " + sDY;
   }
 
 }
